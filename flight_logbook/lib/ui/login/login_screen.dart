7f03a0b8fdd23693e4bb6f9957a59a32dbcd49c7
Makoto Yamazaki <makoto1975@gmail.com>
2020-03-07 12:49:46 +0900

    ログイン画面の色を青くしました。

--- flight_logbook/lib/ui/login/login_screen.dart
+++ flight_logbook/lib/ui/login/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flightlogbook/bloc/login/login_bloc.dart';
 import 'package:flightlogbook/bloc/login/login_event.dart';
 import 'package:flightlogbook/bloc/login/login_repository.dart';
 import 'package:flightlogbook/bloc/login/login_state.dart';
+import 'package:flightlogbook/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -18,52 +19,47 @@ class LoginScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: _appBar,
-      body: BlocBuilder<LoginBloc, LoginState>(
-        bloc: loginBloc,
-        builder: (context, state) {
-          if (state is LoginLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is LoginSuccess) {
-            () async {
-              await Future.delayed(
-                  const Duration(seconds: 2), () => authBloc.add(LoggedIn()));
-            }();
+      body: Container(
+        color: const Color(MOHICAN_BLUE),
+        child: BlocBuilder<LoginBloc, LoginState>(
+          bloc: loginBloc,
+          builder: (context, state) {
+            if (state is LoginLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is LoginSuccess) {
+              Future.delayed(const Duration(milliseconds: 10),
+                  () => authBloc.add(LoggedIn()));
+              return Container();
+            } else if (state is LoginFailure) {
+              return const Center(
+                child: Text('Failure'),
+              );
+            }
+
+            // state == LoginInitial
             return Center(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
-                children: const [
-                  const Text('Success'),
+                children: <Widget>[
+                  RaisedButton.icon(
+                    icon: Icon(
+                      FontAwesomeIcons.google,
+                      color: Colors.white,
+                    ),
+                    label: const Text(
+                      'Login with google',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: const Color(TRITON_BLUE),
+                    onPressed: () => loginBloc.add(LoginWithGoogleOnPressed()),
+                  ),
                 ],
               ),
             );
-          } else if (state is LoginFailure) {
-            return const Center(
-              child: Text('Failure'),
-            );
-          }
-
-          // state == LoginInitial
-          return Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                RaisedButton.icon(
-                  onPressed: () => loginBloc.add(LoginWithGoogleOnPressed()),
-                  icon: Icon(
-                    FontAwesomeIcons.google,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    'Login with google',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
+          },
+        ),
       ),
     );
   }
