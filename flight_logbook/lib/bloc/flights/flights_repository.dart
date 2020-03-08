1f9dbbf6dc16dab86a5ff796a0f7a7a954d89eea
Makoto Yamazaki <makoto1975@gmail.com>
2020-03-08 14:29:17 +0900

    FlightsRepositoryにフライトの追加と削除を実装しました。

--- flight_logbook/lib/bloc/flights/flights_repository.dart
+++ flight_logbook/lib/bloc/flights/flights_repository.dart
@@ -5,6 +5,10 @@ import 'package:flutter/foundation.dart';
 
 abstract class FlightsRepository {
   Future<List<FlightEntry>> getAllFlights();
+
+  Future<FlightEntry> addFlight(FlightEntry newEntry);
+
+  Future<void> removeFlight(String flightId);
 }
 
 class FirestoreFlightsRepository extends FlightsRepository {
@@ -18,22 +22,66 @@ class FirestoreFlightsRepository extends FlightsRepository {
         _firestore = firestore ?? Firestore.instance,
         _authRepository = authRepository;
 
+  static String flightsCollectionPathOf({
+    @required String uid,
+  }) =>
+      "/users/${uid}/flights";
+
+  static String flightDocumentPathOf({
+    @required String uid,
+    @required String flightId,
+  }) =>
+      "${flightsCollectionPathOf(uid: uid)}/${flightId}";
+
   @override
   Future<List<FlightEntry>> getAllFlights() async {
     final currentUser = await _authRepository.getCurrentUser();
 
     // TODO orderByで順序を指定する
     return _firestore
-        .collection("/users/${currentUser.id}/flights")
+        .collection(flightsCollectionPathOf(uid: currentUser.id))
         .where(FlightEntry.FIELD_UID, isEqualTo: currentUser.id)
         .getDocuments()
         .then(
       (QuerySnapshot snapshot) {
         return snapshot.documents
-            .map((DocumentSnapshot entry) =>
-                FlightEntry(id: entry.documentID, data: entry.data))
+            .map(
+              (DocumentSnapshot entry) =>
+              FlightEntry(
+                id: entry.documentID,
+                data: entry.data,
+              ),
+        )
             .toList();
       },
     );
+  }
+
+  @override
+  Future<FlightEntry> addFlight(FlightEntry newEntry) async {
+    final currentUser = await _authRepository.getCurrentUser();
+
+    final data = Map.from(newEntry.data);
+    data[FlightEntry.FIELD_UID] = currentUser.id;
+    final DocumentReference createdDocRef = await Firestore.instance
+        .collection(flightsCollectionPathOf(uid: currentUser.id))
+        .add(data);
+    return FlightEntry(
+      id: createdDocRef.documentID,
+      data: data,
+    );
+  }
+
+  @override
+  Future<void> removeFlight(String flightId) async {
+    final currentUser = await _authRepository.getCurrentUser();
+    await _firestore
+        .document(
+      flightDocumentPathOf(
+        uid: currentUser.id,
+        flightId: flightId,
+      ),
+    )
+        .delete();
   }
 }
